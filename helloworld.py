a278e08e8f0bb0cf2344b9e1495fc84eeb051e3e
CoLoDot <colodotgo@gmail.com>
2020-01-10 23:04:35 +0100

    Ref

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,31 @@
 import spacy
 
-# available languages : fr or en
 
 class Nlp:
+    """'Nlp' is for Natural Language Processing.
+    AVAILABLE LANGUAGES : french or english"""
 
     def __init__(self, lang: str):
+        """Nlp.__init__() constructs
+        an object by loading the
+        choosen language : 'fr' or 'en'"""
         if lang == 'fr':
             self.lang = spacy.load('fr_core_news_sm')
         elif lang == 'en':
             self.lang = spacy.load('en_core_web_sm')
 
     def french(self, sentence: str) -> []:
+        """Nlp.french() takes an str obj and returns a list"""
         fr = [(word.text, word.pos_) for word in self.lang(sentence)]
         return fr
 
     def english(self, sentence: str) -> []:
+        """Nlp.english() takes an str obj and returns a list"""
         en = [(word.text, word.pos_) for word in self.lang(sentence)]
         return en
 
     def get_index(self, sentence: str) -> str:
+        """Nlp.get_index() takes an str obj and returns a str"""
         treat = self.lang(sentence)
         token = treat[1]
         return token.text
--- test.py
+++ test.py
@@ -2,21 +2,27 @@ import unittest
 from helloworld import Nlp
 
 
-class MyTestCase(unittest.TestCase):
+class TestNlp(unittest.TestCase):
+    """Test Nlp class"""
+
     def test_french(self):
+        """Test Nlp.french() returns a list"""
         lang = Nlp('fr')
         french = lang.french('Coucou, ça va ?')
         self.assertIsInstance(french, list)
 
     def test_english(self):
+        """Test Nlp.english() returns a list"""
         lang = Nlp('en')
         en = lang.english('hello, whats up ?')
         self.assertIsInstance(en, list)
 
     def test_get_index(self):
+        """Test Nlp.get_index() returns str"""
         lang = Nlp('en')
         treat = lang.get_index('hi i am a kitty')
         self.assertIsInstance(treat, str)
+
 
 if __name__ == '__main__':
     unittest.main()
